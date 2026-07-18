71b16adde0f2083cd7cefcf02393135ed78cea4b
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-18 14:36:02 +0530

    Renamed PAC-Task1 to PAC-Test and added new PAC-Test1.

---
+++ PACS-Task1/data-insertion.sql
@@ -0,0 +1,5 @@
+insert into student values 
+(1001, 'Rohit', 'CS', 12000),
+(1002, 'Mohit', 'CS', 12000),
+(1003, 'Shobhi', 'IT', 15000),
+(1004, 'Mudit', 'IT', 15000);
---
+++ PACS-Task1/table-creation.sql
@@ -0,0 +1,7 @@
+-- Student
+create table student(
+    rollno int,
+    name varchar(50),
+    branch varchar(100),
+    fee int
+);
