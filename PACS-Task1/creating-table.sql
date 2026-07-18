1f82b84647aae35fcf6a6f82f7379908411476b4
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-18 12:04:18 +0530

    Create SQL tables for clients, employees, and projects

---
+++ PACS-Task1/creating-table.sql
@@ -0,0 +1,42 @@
+-- Clients table
+create table clients(
+  client_id int(4) primary key,
+  cname varchar(20) not null,
+  address varchar(30),
+  email varchar(20) unique,
+  phone varchar(12),
+  business varchar(20) not null
+);
+
+-- Employees table
+create table employees(
+  empno int primary key,
+  ename varchar(20) not null,
+  job varchar(15),
+  salary int check (salary > 0),
+);
+
+-- Projects
+create table projects(
+  project_id int primary key,
+  descr varchar(30) not null,
+  start_date date,
+  planned_end_date date,
+  actual_end_date date,
+  budget int check (budget > 0),
+  client_id int,
+  foreign key (client_id) references clients(client_id),
+  check (actual_end_date > planned_end_date)
+);
+
+-- EmpProjectTasks
+create table empProjectTasks(
+  project_id int(3) primary key,
+  empno int(4),
+  start_date date,
+  end_date date,
+  task varchar(25) not null,
+  status varchar(15) not null,
+  foreign key (project_id) references projects(project_id),
+  foreign key (empno) references employees(empno)
+);
