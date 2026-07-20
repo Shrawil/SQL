c31d8e42b950e1b6e221a201637250d60f61955d
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-20 19:03:09 +0530

    Changing order of inserting data to avoid Foreign key error.

--- PACS-Test/inserting-data.sql
+++ PACS-Test/inserting-data.sql
@@ -5,21 +5,6 @@ INSERT INTO clients VALUES
 (1003, 'MoneySaver Distributors', 'Kolkata', 'save@moneysaver.com', '7799886655', 'Reseller'),
 (1004, 'Lawful Corp', 'Chennai', 'justice@lawful.com', '9210342219', 'Professional');
 
--- EmpProjectTasks
-INSERT INTO empprojecttasks VALUES
-(401, 7001, '2011-04-01', '2011-04-20', 'System Analysis', 'Completed'),
-(401, 7002, '2011-04-21', '2011-05-30', 'System Design', 'Completed'),
-(401, 7003, '2011-06-01', '2011-07-15', 'Coding', 'Completed'),
-(401, 7004, '2011-07-18', '2011-09-01', 'Coding', 'Completed'),
-(401, 7006, '2011-09-03', '2011-09-15', 'Testing', 'Completed'),
-(401, 7009, '2011-09-18', '2011-10-05', 'Code Change', 'Completed'),
-(401, 7008, '2011-10-06', '2011-10-16', 'Testing', 'Completed'),
-(401, 7007, '2011-10-06', '2011-10-22', 'Documentation', 'Completed'),
-(401, 7011, '2011-10-22', '2011-10-31', 'Sign Off', 'Completed'),
-(402, 7010, '2011-08-01', '2011-08-20', 'System Analysis', 'Completed'),
-(402, 7002, '2011-08-22', '2011-09-30', 'System Design', 'Completed'),
-(402, 7004, '2011-10-01', NULL, 'Coding', 'In Progress');
-
 -- Employees
 INSERT INTO employees VALUES
 (7001, 'Sandeep', 'Analyst', 25000, 10),
@@ -40,3 +25,18 @@ INSERT INTO projects VALUES
 (402, 'Accounting', '2011-08-01', '2012-01-01', NULL, 500000, 1002),
 (403, 'Payroll', '2011-10-01', '2011-12-31', NULL, 75000, 1003),
 (404, 'Contact mgmt', '2011-11-01', '2011-12-31', NULL, 50000, 1004);
+
+-- EmpProjectTasks
+INSERT INTO empprojecttasks VALUES
+(401, 7001, '2011-04-01', '2011-04-20', 'System Analysis', 'Completed'),
+(401, 7002, '2011-04-21', '2011-05-30', 'System Design', 'Completed'),
+(401, 7003, '2011-06-01', '2011-07-15', 'Coding', 'Completed'),
+(401, 7004, '2011-07-18', '2011-09-01', 'Coding', 'Completed'),
+(401, 7006, '2011-09-03', '2011-09-15', 'Testing', 'Completed'),
+(401, 7009, '2011-09-18', '2011-10-05', 'Code Change', 'Completed'),
+(401, 7008, '2011-10-06', '2011-10-16', 'Testing', 'Completed'),
+(401, 7007, '2011-10-06', '2011-10-22', 'Documentation', 'Completed'),
+(401, 7011, '2011-10-22', '2011-10-31', 'Sign Off', 'Completed'),
+(402, 7010, '2011-08-01', '2011-08-20', 'System Analysis', 'Completed'),
+(402, 7002, '2011-08-22', '2011-09-30', 'System Design', 'Completed'),
+(402, 7004, '2011-10-01', NULL, 'Coding', 'In Progress');
